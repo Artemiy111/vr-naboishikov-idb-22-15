4a93e6179a7312ced17fe4fc12ecaecda4cb4d37
Artemiy111 <mrart111@mail.ru>
2023-10-05 17:04:55 +0300

    chore

--- Assets/Ui/UIHandler.cs
+++ Assets/Ui/UIHandler.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.UIElements;
 public class UIHandler : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject cube; 
+    public GameObject cube;
 
     private void OnEnable()
     {
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
         Button button = root.Q<Button>("MyButton");
 
-        button.clicked += () => {
-            if (cube.activeSelf) {cube.SetActive(false);} else cube.SetActive(true);
+        button.clicked += () =>
+        {
+
+            if (cube.activeSelf)
+            {
+                cube.SetActive(false);
+            }
+            else cube.SetActive(true);
         };
-    } 
+    }
 }
